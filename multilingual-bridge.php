bd9638aca56cf1d46db847acc899ded5045f55ce
Justin Vogt <mail@justin-vogt.de>
2025-06-28 14:46:55 +0200

    Add 'Requires Plugins' header to declare WPML dependency
    Added the 'Requires Plugins: sitepress-multilingual-cms' header to the plugin file to explicitly declare the dependency on WPML. This helps WordPress understand the plugin requirements and improves plugin management.

--- multilingual-bridge.php
+++ multilingual-bridge.php
@@ -22,6 +22,7 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       multilingual-bridge
  * Domain Path:       /languages
+ * Requires Plugins: sitepress-multilingual-cms
  */
 
 // If this file is called directly, abort.
